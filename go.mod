b2226702a5aed11921f1ec830db87d945fe88b65
samirhembrom <samirhembrom007@gmail.com>
2025-04-23 19:30:29 +0530

    server created

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/samirhembrom/chirpy
+
+go 1.24.2
---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	mux := http.NewServeMux()
+	server := http.Server{
+		Addr:    ":8080",
+		Handler: mux,
+	}
+	err := server.ListenAndServe()
+	if err != nil {
+		log.Printf("err %v", err)
+	}
+}
