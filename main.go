83e43753ac648a17dbff71817b561cbfdae02e58
samirhembrom <samirhembrom007@gmail.com>
2025-04-25 10:40:57 +0530

    create handler func for hits

--- main.go
+++ main.go
@@ -1,17 +1,38 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"sync/atomic"
 )
 
-func main() {
-	const fileRootPath = "."
-	const port = "8080"
+const fileRootPath = "."
 
+const port = "8080"
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		log.Printf("Request received, hits now: %d", cfg.fileserverHits.Load())
+		next.ServeHTTP(w, r)
+	})
+}
+
+func main() {
+	cfg := &apiConfig{}
 	mux := http.NewServeMux()
-	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir(fileRootPath))))
+	mux.Handle(
+		"/app/",
+		cfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(fileRootPath)))),
+	)
 	mux.HandleFunc("/healthz", handlerReadiness)
+	mux.HandleFunc("/metrics", cfg.handlerWriteRequests())
+	mux.HandleFunc("/reset", cfg.handlerResetRequests())
 
 	srv := &http.Server{
 		Addr:    ":" + port,
@@ -26,4 +47,22 @@ func handlerReadiness(w http.ResponseWriter, req *http.Request) {
 	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(200)
 	w.Write([]byte("OK"))
+}
+
+func (cfg *apiConfig) handlerWriteRequests() func(w http.ResponseWriter, req *http.Request) {
+	return func(w http.ResponseWriter, req *http.Request) {
+		var count int32 = cfg.fileserverHits.Load()
+		str := fmt.Sprintf("Hits: %d", count)
+		w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(200)
+		w.Write([]byte(str))
+	}
+}
+
+func (cfg *apiConfig) handlerResetRequests() func(w http.ResponseWriter, req *http.Request) {
+	return func(w http.ResponseWriter, req *http.Request) {
+		cfg.fileserverHits.Store(0)
+		w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(200)
+	}
 }
