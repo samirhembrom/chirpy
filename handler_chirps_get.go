eca4d60d50d82743043b0e621259525d0ce40984
samirhembrom <samirhembrom007@gmail.com>
2025-05-11 00:26:00 +0530

    ch9_l1

--- handler_chirps_get.go
+++ handler_chirps_get.go
@@ -8,6 +8,31 @@ import (
 )
 
 func (cfg *apiConfig) handlerChirpsRetrieve(w http.ResponseWriter, req *http.Request) {
+	s := req.URL.Query().Get("author_id")
+	if s != "" {
+		user_id, err := uuid.Parse(s)
+		if err != nil {
+			respondWithError(w, http.StatusInternalServerError, "Couldn't parse auther id", err)
+			return
+		}
+		chirpsDb, err := cfg.db.ListChirpsByUser(context.Background(), user_id)
+		if err != nil {
+			respondWithError(w, http.StatusInternalServerError, "Couldn't fetch chirps", err)
+			return
+		}
+		chirps := []Chirp{}
+		for _, chirp := range chirpsDb {
+			chirps = append(chirps, Chirp{
+				ID:         chirp.ID,
+				Created_At: chirp.CreatedAt,
+				Updated_At: chirp.UpdatedAt,
+				Body:       chirp.Body,
+				User_Id:    chirp.UserID,
+			})
+		}
+		respondWithJSON(w, http.StatusOK, chirps)
+		return
+	}
 	chirpsDb, err := cfg.db.ListChirps(context.Background())
 	if err != nil {
 		respondWithError(w, http.StatusInternalServerError, "Couldn't fetch chirps", err)
